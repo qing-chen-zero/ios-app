a26213f6f13773986513793c13a836a830d8061d
quqingchen <18808173531@163.com>
2022-04-12 17:54:00 +0800

    init

---
+++ test01/test01/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  test01
+//
+//  Created by Qingchen on 2022/4/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ test01/test01/test01App.swift
@@ -0,0 +1,17 @@
+//
+//  test01App.swift
+//  test01
+//
+//  Created by Qingchen on 2022/4/12.
+//
+
+import SwiftUI
+
+@main
+struct test01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
